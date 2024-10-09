ab41b7c542a8d71636b1854fdeafdce72a078cf4
Pranav Krishna <pkrish05@calpoly.edu>
2024-10-09 10:30:08 -0700

    Added fetch connection to backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,8 +1,8 @@
 // src/MyApp.jsx
-import { useState } from "react";
-import React from "react";
 import Table from "./Table";
 import Form from "./Form";
+import {React, useState, useEffect} from 'react';
+
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
@@ -14,9 +14,21 @@ function MyApp() {
         setCharacters(updated);
     }
 
+    function fetchUsers() {
+        const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
+
     function updateList(person) {
         setCharacters([...characters, person]);
     }
+
+    useEffect(() => {
+        fetchUsers()
+            .then((res) => res.json())
+            .then((json) => setCharacters(json["users_list"]))
+            .catch((error) => { console.log(error); });
+      }, [] );
 
     return (
         <div className="container">
